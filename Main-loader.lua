005bf705bc0681d915406c2077f6d6c72a71ae0d
Hajipak <mohammadalghozali91@gmail.com>
2025-10-24 12:35:46 +0700

    Main-loader.lua

--- Main-loader.lua
+++ Main-loader.lua
@@ -1,12 +1,12 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/Universal/Fps%20display.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/File/Fps%20display.lua"))()
 local PlaceScripts = {
     [10324346056] = { 
         name = "Big Team", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/DaraHub-Evade.lua" 
+        url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/Script/Evade%20Test.lua" 
     },
     [9872472334] = { 
         name = "Evade", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/DaraHub-Evade.lua" 
+        url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/Script/Evade%20Test.lua" 
     },
     [96537472072550] = { 
         name = "Legacy Evade", 
@@ -14,37 +14,33 @@ local PlaceScripts = {
     },
     [10662542523] = { 
         name = "Casual", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/DaraHub-Evade.lua" 
+        url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/Script/Evade%20Test.lua" 
     },
     [10324347967] = { 
         name = "Social Space", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/DaraHub-Evade.lua" 
+        url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/Script/Evade%20Test.lua" 
     },
     [121271605799901] = { 
         name = "Player Nextbots", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/DaraHub-Evade.lua" 
+        url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/Script/Evade%20Test.lua" 
     },
     [10808838353] = { 
         name = "VC Only", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/DaraHub-Evade.lua" 
+        url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/Script/Evade%20Test.lua" 
     },
     [11353528705] = { 
         name = "Pro", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/DaraHub-Evade.lua" 
+        url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/Script/Evade%20Test.lua" 
     },
     [99214917572799] = { 
         name = "Custom Servers", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/evade/DaraHub-Evade.lua" 
-    }, --MM2
-     [142823291] = { 
-        name = "Murder Mystery 2", 
-        url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/MM2/DaraHub-MM2.lua" 
+        url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/Script/Evade%20Test.lua"  
     },
 }
 
 local UniversalScript = {
     name = "Universal Script",
-    url = "https://raw.githubusercontent.com/Pnsdgsa/Script-kids/refs/heads/main/Scripthub/Darahub/Universal/Darahub-Universal.lua"
+    url = "https://raw.githubusercontent.com/Hajipak/Evade/refs/heads/main/File/Universal-Hub.lua"
 }
 
 local currentGameId = game.PlaceId
